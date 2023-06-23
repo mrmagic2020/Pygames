2c2cfa88bb8654b9a32d81c275ad921d5ba156e4
mrmagic2020 <2732298611@qq.com>
2023-06-23 16:54:19 +1000

    add title page

--- Games/Tic Tac Toe/graphics.py
+++ Games/Tic Tac Toe/graphics.py
@@ -2,20 +2,33 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import os, pygame
+from enum import Enum
 
 if TYPE_CHECKING:
     import logic
 
 
+pygame.font.init()
+
 piece_size = (200, 200)
 board_size = window_size = (800, 800)
+start_button_size = (200, 80)
 img_path = {
     "Placeholder": os.path.join("Assets", "Placeholder.png"),
     "Cross": os.path.join("Assets", "cross.png"),
     "Circle": os.path.join("Assets", "circle.png"),
     "Board": os.path.join("Assets", "board.png"),
+    "Buttons": {
+        "button_rect_fillet_blue": os.path.join(
+            "Assets", "Buttons", "button_rect_fillet_blue.png"
+        ),
+        "button_rect_fillet_blue_hover": os.path.join(
+            "Assets", "Buttons", "button_rect_fillet_blue_hover.png"
+        ),
+    },
 }
-colours = {"pale_turquoise": (175, 238, 238)}
+colors = {"black": (0, 0, 0), "pale_turquoise": (175, 238, 238)}
+draw = {"rect": pygame.draw.rect}
 
 
 def init() -> pygame.Surface:
@@ -24,7 +37,14 @@ def init() -> pygame.Surface:
     """
     global screen
     screen = pygame.display.set_mode(window_size)
+    pygame.display.set_caption("Tic Tac Toe")
     return screen
+
+
+def clearScreen(background="pale_turquoise") -> None:
+    screen.fill(colors[background])
+    pygame.display.flip()
+    return None
 
 
 def getPos(index: int) -> tuple[int, int]:
@@ -55,7 +75,10 @@ def getPos(index: int) -> tuple[int, int]:
 
 class Placeholder:
     def __init__(
-        self, index: int = -1, image=pygame.image.load(img_path["Placeholder"]), scale=piece_size
+        self,
+        index: int = -1,
+        image=pygame.image.load(img_path["Placeholder"]),
+        scale=piece_size,
     ) -> None:
         self.index = index
         self.image = pygame.transform.scale(image, scale)
@@ -65,7 +88,10 @@ class Placeholder:
 
 class Cross:
     def __init__(
-        self, index: int = -1, image=pygame.image.load(img_path["Cross"]), scale=piece_size
+        self,
+        index: int = -1,
+        image=pygame.image.load(img_path["Cross"]),
+        scale=piece_size,
     ) -> None:
         self.index = index
         self.image = pygame.transform.scale(image, scale)
@@ -78,7 +104,10 @@ class Cross:
 
 class Circle:
     def __init__(
-        self, index: int = -1, image=pygame.image.load(img_path["Circle"]), scale=piece_size
+        self,
+        index: int = -1,
+        image=pygame.image.load(img_path["Circle"]),
+        scale=piece_size,
     ) -> None:
         self.index = index
         self.image = pygame.transform.scale(image, scale)
@@ -98,9 +127,10 @@ class Board:
     ) -> None:
         self.grid = grid
         self.image = pygame.transform.scale(image, scale)
-    
+
     def render(self) -> None:
-        screen.fill(colours["pale_turquoise"])
+        """Draws the board onto the screen. Does not update display."""
+        screen.fill(colors["pale_turquoise"], self.image.get_rect())
         screen.blit(self.image, self.image.get_rect())
 
         for piece in self.grid.pieces:
@@ -112,5 +142,105 @@ class Board:
             #     screen.blit(placeholder.image, placeholder.rect)
             # index += 1
 
-        pygame.display.flip()
+        # pygame.display.flip()
+        return None
+
+
+class Text:
+    """
+    `content` the text being displayed
+
+    `font` the font being used
+
+    `pos` the position relative to `surface`
+
+    `surface` the surface the text will be rendered on
+    """
+
+    def __init__(
+        self,
+        content: str,
+        font: pygame.font.Font,
+        pos: tuple[int, int],
+        surface: pygame.Surface,
+        color: tuple[int, int, int] = colors["black"],
+        background: pygame.Color = None,
+    ) -> None:
+        self.content = content
+        self.pos = pos
+        self.font = font
+        self.color = color
+        self.background = background
+        self.surface = surface
+        return None
+
+    def render(self) -> None:
+        """Draws the text onto the screen. Does not update display."""
+        surf = self.font.render(self.content, True, self.color, self.background)
+        self.surface.blit(surf, self.pos)
+        return None
+
+
+class Interactive(Enum):
+    BUTTON = 0
+    BUTTON_ANIMATED = 1
+
+
+class Button:
+    def __init__(
+        self,
+        scale: tuple[int, int],
+        pos: tuple[int, int],
+        image: pygame.Surface = pygame.image.load(
+            img_path["Buttons"]["button_rect_fillet_blue"]
+        ),
+    ) -> None:
+        self.type = Interactive.BUTTON
+        self.image = pygame.transform.scale(image, scale)
+        self.rect = pygame.rect.Rect(pos, scale)
+        self.pos = pos
+        return None
+
+    def render(self) -> None:
+        """Draws the button onto the screen. Does not update display."""
+        screen.fill(colors["pale_turquoise"], pygame.rect.Rect(self.rect))
+        screen.blit(self.image, self.pos)
+        return None
+
+
+class AnimatedButton(Button):
+    def __init__(
+        self,
+        scale: tuple[int, int],
+        pos: tuple[int, int],
+        image: pygame.Surface = pygame.image.load(
+            img_path["Buttons"]["button_rect_fillet_blue"]
+        ),
+        hover_image: pygame.Surface = pygame.image.load(
+            img_path["Buttons"]["button_rect_fillet_blue_hover"]
+        ),
+    ) -> None:
+        super().__init__(scale, pos, image)
+        self.type = Interactive.BUTTON_ANIMATED
+        self.isHover = False
+        self.hover_image = pygame.transform.scale(hover_image, scale)
+        return None
+
+    def render(self) -> None:
+        screen.fill(colors["pale_turquoise"], pygame.rect.Rect(self.rect))
+        image = self.image
+        if self.isHover:
+            image = self.hover_image
+        screen.blit(image, self.pos)
+        return None
+
+    def reset(self) -> pygame.Surface:
+        self.isHover = False
+        return self.image
+
+    def hover(self) -> pygame.Surface:
+        self.isHover = True
+        return self.hover_image
+
+    def press(self) -> pygame.Surface:
         return None
--- Games/Tic Tac Toe/main.py
+++ Games/Tic Tac Toe/main.py
@@ -2,32 +2,83 @@ import sys, pygame
 import graphics, logic
 
 pygame.init()
+pygame.font.init()
 
 
-def game():
-    screen = graphics.init()
-    clock = pygame.time.Clock()
+screen = graphics.init()
+clock = pygame.time.Clock()
 
-    board = graphics.Board(logic.Grid())
-    # dragging = False
+
+def quit():
+    pygame.quit()
+    sys.exit()
+
+
+def title() -> None:
+    title_text = graphics.Text(
+        "Tic Tac Toe", pygame.font.SysFont("papyrus", 140), (150, 50), screen
+    )
+    start_button = graphics.AnimatedButton(
+        graphics.start_button_size,
+        ((graphics.window_size[0] - graphics.start_button_size[0]) / 2, 400),
+    )
+    start_text = graphics.Text(
+        "CLASSIC", pygame.font.SysFont("papyrus", 50), (20, 7), start_button.image
+    )
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                quit()
+            if event.type == pygame.MOUSEMOTION:
+                if start_button.rect.collidepoint(pygame.mouse.get_pos()):
+                    start_text.surface = start_button.hover()
+                else:
+                    start_button.reset()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                    if start_button.rect.collidepoint(pygame.mouse.get_pos()):
+                        game()
+                        break
+
+        screen.fill(graphics.colors["pale_turquoise"])
+        title_text.render()
+        start_button.render()
+        start_text.render()
+        pygame.display.flip()
+        clock.tick(60)
+    return None
+
+
+def game() -> None:
+    """Start a new pvp tic tac toe classic game."""
+
+    board = graphics.Board(logic.Grid())
+    board.grid.reset()
+    # dragging = False
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if pygame.mouse.get_pressed(num_buttons=3)[0]:
                     for i in range(9):
                         piece = board.grid.pieces[i]
-                        if piece.alias == graphics.Placeholder().alias and pygame.Rect(piece.pos, graphics.piece_size).collidepoint(pygame.mouse.get_pos()):
+                        if piece.alias == graphics.Placeholder().alias and pygame.Rect(
+                            piece.pos, graphics.piece_size
+                        ).collidepoint(pygame.mouse.get_pos()):
                             if not board.grid.winner():
                                 board.grid.move(i)
-                    if board.grid.tie(): print("It's a tie!")
-                    else: print("The winner is", board.grid.winner()) if board.grid.winner() is not None else None
+                    if board.grid.tie():
+                        print("It's a tie!")
+                    else:
+                        print(
+                            "The winner is", board.grid.winner()
+                        ) if board.grid.winner() is not None else None
             if event.type == pygame.KEYDOWN:
                 if pygame.key.get_pressed()[pygame.K_ESCAPE]:
-                    print("New Game...")
+                    print("Exit to menu...")
                     board.grid.reset()
+                    title()
+                    break
 
             # if event.type == pygame.KEYDOWN:
             #     print(board.grid.winner())
@@ -40,14 +91,17 @@ def game():
         #     elif event.type == pygame.MOUSEBUTTONUP:
         #         if not pygame.mouse.get_pressed(num_buttons=3)[0]:
         #             print(cross.pos)
-        
+
         # if not pygame.mouse.get_pressed(num_buttons=3)[0]:
         #     dragging = False
         # if dragging:
         #     mouse_x, mouse_y = pygame.mouse.get_pos()
         #     cross.pos = mouse_x - offset_x, mouse_y - offset_y
-        
-        board.render()
-        clock.tick(30)
 
-game()
+        board.render()
+        pygame.display.flip()
+        # clock.tick(120)
+    return None
+
+
+title()
